8f4f992c3aba6d9129e24cd90707453c3ce08c71
Yuri <creator.and.dev@gmail.com>
2018-02-02 00:39:54 +0200

    //fix

---
+++ app/src/main/java/yuri/kl/com/ua/menu/MainActivity.java
@@ -0,0 +1,37 @@
+package yuri.kl.com.ua.menu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void printToLogs(View view) {
+        // Find first menu item TextView and print the text to the logs
+        TextView firstMenuItem1 = (TextView) findViewById(R.id.menu_item_1);
+//        Log.i("MainActivity", (String) firstMenuItem1.getText());
+        String v1 = firstMenuItem1.toString();
+        Log.i("MainActivity", v1);
+
+        // Find second menu item TextView and print the text to the logs
+        TextView firstMenuItem2 = (TextView) findViewById(R.id.menu_item_2);
+//        Log.i("MainActivity", (String) firstMenuItem2.getText());
+        String v2 = firstMenuItem1.toString();
+        Log.i("MainActivity", v2);
+
+        // Find third menu item TextView and print the text to the logs
+        TextView firstMenuItem3 = (TextView) findViewById(R.id.menu_item_3);
+//        Log.i("MainActivity", (String) firstMenuItem3.getText());
+        String v3 = firstMenuItem1.toString();
+        Log.i("MainActivity", v3);
+    }
+}
